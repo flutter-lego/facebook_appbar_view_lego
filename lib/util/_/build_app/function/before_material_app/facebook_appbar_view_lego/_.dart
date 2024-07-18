e17ae9861310c33babbdb32b433d333e85880700
june lee <melodysdreamj@gmail.com>
2024-07-18 16:10:28 +0900

    Initial commit

---
+++ lib/util/_/build_app/function/before_material_app/facebook_appbar_view_lego/_.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../../main.dart';
+
+// @ReadyBeforeMaterialApp()
+// Future<void> readyForFacebookAppbarViewLego(BuildContext context) async {}
---
+++ lib/util/_/build_app/widget/my_app/facebook_appbar_view_lego/_.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import '../../../../../../main.dart';
+
+// @CoverMyApp()
+// Widget Function() coverFacebookAppbarViewLego(Widget Function() materialAppBuilder) {
+//   return () => materialAppBuilder();
+// }
